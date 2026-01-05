af8f9080a8923944ffaff8604fe4aff643459d44
surya143s6 <suriyarangan2017@gmail.com>
2026-01-05 17:31:15 +0530

    new

---
+++ Day_15/practice.js
@@ -0,0 +1,22 @@
+const movies = [
+  {
+    id: 1,
+    title: "avengers",
+    language: "English",
+    ratings: 4.5,
+  },
+
+  { id: 2, title: "avatar", language: "English", ratings: 4.0 },
+  {
+    id: 3,
+    title: "Stranger things",
+    language: "English",
+    ratings: 4.6,
+  },
+  { id: 4, title: "doctor", language: "korean", ratings: 4.8 },
+];
+
+const getMovieRatings = (movies, id) => {};
+
+console.log(getMovieRatings(movies, 4));
+3;
