b42b3d693afe8ccc9815f0247b83621f98d39451
surya143s6 <suriyarangan2017@gmail.com>
2026-02-24 13:00:47 +0530

    task-1.1 flower

--- Day_31/test.py
+++ Day_31/test.py
@@ -15,14 +15,32 @@
 
 # emoji(4, "🥕")
 
-def emoji(rows,symbol="🔥"):
-    for i in range(rows+1):
-        print(i* symbol)
+# def emoji(rows,symbol="🔥"):
+#     for i in range(rows+1):
+#         print(i* symbol)
 
-emoji(2, "🍓")
-emoji(6, "🍍")
-emoji(4, "🥕")
-emoji(3)
+# emoji(2, "🍓")
+# emoji(6, "🍍")
+# emoji(4, "🥕")
+# emoji(3)
 
 
+    
+
+# Task 1.1 - Print all the flower using for loop
+# Hint - range, len
+
+
+flowers = ["💐", "🌷", "🌼", "🌷", "🌺"]
+
+for i in range(len(flowers)):
+    print("flower", i+1, "-", flowers[i])
+    
+
+# Expected output
+# Flower 1 - 💐
+# Flower 2 - 🌷
+# Flower 3 - 🌼
+# Flower 4 - 🌷
+# Flower 5 - 🌺
 
