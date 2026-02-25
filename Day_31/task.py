02b9f15d18a0d3dd85b784b66a9c37d3320ece49
surya143s6 <suriyarangan2017@gmail.com>
2026-02-25 10:26:04 +0530

    highest-rating-book-task

---
+++ Day_31/task.py
@@ -0,0 +1,15 @@
+books = [
+    {"title": "Infinite Jest", "rating": 4.5, "genre": "Fiction"},
+    {"title": "The Catcher in the Rye", "rating": 3.9, "genre": "Fiction"},
+    {"title": "Sapiens", "rating": 4.9, "genre": "History"},
+    {"title": "A Brief History of Time", "rating": 4.8, "genre": "Science"},
+    {"title": "Clean Code", "rating": 4.7, "genre": "Technology"},
+]
+
+highest_rated = []
+
+for book in books:
+    if book["rating"] >= 4.7:
+        highest_rated.append(book["title"])
+
+print("Highest rated books are", ", ".join(highest_rated))
